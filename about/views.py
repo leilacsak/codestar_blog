fb2cb1ae2364414082f4831d5365521ee9ec4e6f
leilacsak <leilacsak@gmail.com>
2026-02-03 14:54:28 +0000

    Add About app with model, views, templates, and admin configuration

---
+++ about/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import About
+
+
+# Create your views here.
+
+def about_me(request):
+    """
+    Display the About page.
+
+    **Context**
+
+    ``about``
+        An instance of :model:`about.About`.
+
+    **Template:**
+
+    :template:`about/about.html`
+    """
+
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
