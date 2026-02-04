4f3003e495f91f92170c6589a98f52c322cba220
leilacsak <leilacsak@gmail.com>
2026-02-04 13:50:45 +0000

    Refactor post_detail view to improve comment form handling and logging

--- blog/views.py
+++ blog/views.py
@@ -30,20 +30,24 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
- 
+    comment_form = CommentForm()
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
+        
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            
         messages.add_message(
             request, messages.SUCCESS,
             'Comment submitted and awaiting approval'
         )
 
-    comment_form = CommentForm()
+        comment_form = CommentForm()
+    print("About to render the post detail template")
 
     return render(
         request,
